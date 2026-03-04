bea484dab89b1cb9dd2ae6f637753707df8eb1d9
mojimoon <cryovit@outlook.com>
2026-03-04 14:22:15 +0800

    bugfix

--- Mojimoon-ImperfectHook/mod_main.gd
+++ Mojimoon-ImperfectHook/mod_main.gd
@@ -5,6 +5,6 @@ const MOD_ID = "Mojimoon-ImperfectHook"
 func _init():
     var dir = ModLoaderUtils.get_unpacked_dir() + MOD_ID + "/"
 
-    ModLoaderMod.install_script_extension(dir + "ui/menus/shop/base_shop.gd")
+    ModLoaderMod.install_script_extension(dir + "extensions/ui/menus/shop/base_shop.gd")
     # ModLoaderMod.install_script_extension(dir + "ui/menus/shop/coop_shop.gd")
     # ModLoaderMod.install_script_extension(dir + "progress_data.gd")
